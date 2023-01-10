fa5974b8e4a44583dde89a771665df5c078d8de7
Igor <igor.kiyashko@inbox.ru>
2023-01-10 16:56:13 +0600

    lesson 3

--- main.py
+++ main.py
@@ -11,8 +11,11 @@ if __name__ == '__main__':
     clock= pygame.time.Clock()
 
 
-    heli = pygame.image.load("heli_right.png")
-    heli = pygame.transform.scale(heli,(64,64))
+    heli_right = pygame.image.load("heli_right.png")
+    heli_right = pygame.transform.scale(heli_right,(64,64))
+
+    heli_left = pygame.image.load("heli_left.png")
+    heli_left = pygame.transform.scale(heli_left,(64,64))
 
     background = pygame.image.load("plx-5.png")
     background = pygame.transform.scale(background,(700,500))
@@ -22,11 +25,17 @@ if __name__ == '__main__':
 
     dx = 0
     dy = 0
+    flying_up = False
+    heading_right = True
 
     game_over = False
     while not game_over:
         x = x+dx
         y = y+dy
+        if flying_up:
+            dy = dy-1
+        else:
+            dy = dy+1
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 game_over = True
@@ -35,29 +44,34 @@ if __name__ == '__main__':
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    if dx >0 :
-                        dx = 0
-                    else:
-                        dx = -10
+                    dx = -10
+                    heading_right = False
                 if event.key == pygame.K_RIGHT:
-                    if dx < 0:
-                        dx = 0
-                    else:
-                        dx = 10
+                    dx = 10
+                    heading_right = True
                 if event.key == pygame.K_UP:
+                    flying_up = True
+                if event.key == pygame.K_DOWN:
+                    dy = 10
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    if dx<0:
+                        dx = 0
+                if event.key == pygame.K_RIGHT:
+                    if dx>0:
+                        dx = 0
+                if event.key == pygame.K_UP:
+                    flying_up = False
+                if event.key == pygame.K_DOWN:
                     if dy>0:
                         dy = 0
-                    else:
-                        dy = -10
-                if event.key == pygame.K_DOWN:
-                    if dy<0:
-                        dy = 0
-                    else:
-                        dy = 10
 
         screen.fill((255,255,255))
         screen.blit(background,(0,0))
-        screen.blit(heli,(x,y))
+        if heading_right:
+            screen.blit(heli_right,(x,y))
+        else:
+            screen.blit(heli_left, (x, y))
         pygame.display.flip()
         clock.tick(10)
 
